2390224fb312a0146e4b47c67d46475393a0d599
Aaron Schumacher <ajschumacher@gmail.com>
2013-10-20 13:38:53 -0400

    clean up viz source and re-center/shrink to 500x500

--- js/visualization.js
+++ js/visualization.js
@@ -1,57 +1,16 @@
 $(document).ready(function() {
-//			
-			var packer = sm.packer();
-//			
-//var projection = d3.geo.albersUsa()
-//
-//			var svg = d3.select('#content').append('svg')
-//					.attr('width', 960)
-//					.attr('height', 500);
-//			
-//			var neighborhoods = svg.append('g')
-//					.attr('width', 960)
-//					.attr('height', 500)
-//					.attr('id', 'states');
-//									
-//
-//			var path = d3.geo.path();
-//			d3.json("data/dc.json", function(json) {
-//				neighborhoods.selectAll("path")
-//					.data(json.features)
-//					.enter().append("path")
-//					.attr("d", path);
-//			});
-//			
-//	  svg.append("circle").attr("r",5).attr("transform", function() {return "translate(" + projection([-75, 43]) + ")";});
-//			
-//$.get('data/health.json', function(health){
-//	var scale = d3.scale.linear().domain([0,200]).range([1,40])
-//	health.states.forEach(function(point){
-//		if (point.min_21 !== null){
-//	  svg.append("circle").attr("r",scale(point.min_21)).attr("transform", function() {return "translate(" + projection([point.lon, point.lat]) + ")";});
-//	  packMetros();
-//		}
-//	})
-//})
-//
-//			function packMetros() {
-//			
-//				var elements = d3.selectAll('#content circle')[0];
-//				
-//				packer.elements(elements).start();
-//			
-//			}
 
+var packer = sm.packer();
 
 projection = d3.geo.albersUsa()
-.scale(160000)
-.translate([-41500, 5000]);
+  .scale(160000)
+  .translate([-41630, 4900]);
 path = d3.geo.path().projection(projection)
 
 svg = d3.select("#content")
   .append("svg:svg")
-  .attr("width", 750)
-  .attr("height", 600)
+  .attr("width", 500)
+  .attr("height", 500)
 
 d3.json("data/neighborhood_boundaries.json", function(json) {
   svg.append("svg:g")
@@ -62,7 +21,7 @@ d3.json("data/neighborhood_boundaries.json", function(json) {
     .attr("fill-opacity", 0.5)
 	.attr("fill", "#000")
     .attr("stroke", "#000")
-});	
+});
 
 $.get('data/schools.json', function(point){
 	var scale = d3.scale.linear().domain([0,200]).range([1,5])
@@ -72,14 +31,9 @@ $.get('data/schools.json', function(point){
 	})
 })
 
-			function packMetros() {
-			
-				var elements = d3.selectAll('#content circle')[0];
-				
-				packer.elements(elements).start();
-			
-			}
+function packMetros() {
+	var elements = d3.selectAll('#content circle')[0];
+	packer.elements(elements).start();
+}
 
-//	  svg.append("circle").attr("r",5).attr("transform", function() {return "translate(" + projection([-77.01, 38.91]) + ")";});
-
-});		
+}); // end document ready function
