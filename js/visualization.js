0052f6c0b45d6b49f53fcfb2f9ecd8cae868c55d
nickmcclellan <mcclellan.nick@gmail.com>
2013-10-20 14:20:02 -0400

    Adding click to zoom coding example

--- js/visualization.js
+++ js/visualization.js
@@ -1,46 +1,6 @@
 $(document).ready(function() {
-//			
-			var packer = sm.packer();
-//			
-//var projection = d3.geo.albersUsa()
-//
-//			var svg = d3.select('#content').append('svg')
-//					.attr('width', 960)
-//					.attr('height', 500);
-//			
-//			var neighborhoods = svg.append('g')
-//					.attr('width', 960)
-//					.attr('height', 500)
-//					.attr('id', 'states');
-//									
-//
-//			var path = d3.geo.path();
-//			d3.json("data/dc.json", function(json) {
-//				neighborhoods.selectAll("path")
-//					.data(json.features)
-//					.enter().append("path")
-//					.attr("d", path);
-//			});
-//			
-//	  svg.append("circle").attr("r",5).attr("transform", function() {return "translate(" + projection([-75, 43]) + ")";});
-//			
-//$.get('data/health.json', function(health){
-//	var scale = d3.scale.linear().domain([0,200]).range([1,40])
-//	health.states.forEach(function(point){
-//		if (point.min_21 !== null){
-//	  svg.append("circle").attr("r",scale(point.min_21)).attr("transform", function() {return "translate(" + projection([point.lon, point.lat]) + ")";});
-//	  packMetros();
-//		}
-//	})
-//})
-//
-//			function packMetros() {
-//			
-//				var elements = d3.selectAll('#content circle')[0];
-//				
-//				packer.elements(elements).start();
-//			
-//			}
+
+var packer = sm.packer();
 
 
 projection = d3.geo.albersUsa()
@@ -80,6 +40,6 @@ $.get('data/schools.json', function(point){
 			
 			}
 
-//	  svg.append("circle").attr("r",5).attr("transform", function() {return "translate(" + projection([-77.01, 38.91]) + ")";});
+	  svg.append("circle").attr("r",5).attr("transform", function() {return "translate(" + projection([-77.01, 38.91]) + ")";});
 
 });		
