af8d5704ffa74e21a501e2d6b0699f58e48d1ce9
Aaron Schumacher <ajschumacher@gmail.com>
2013-10-20 15:33:08 -0400

    school radius scales now sqrt and working

--- js/visualization.js
+++ js/visualization.js
@@ -12,7 +12,7 @@ projection = d3.geo.albersUsa()
 
 var path = d3.geo.path().projection(projection)
 
-var svg = d3.select("#content")
+svg = d3.select("#content")
   .append("svg:svg")
   .attr("width", width)
   .attr("height", height)
@@ -40,10 +40,10 @@ d3.json("data/neighborhood_boundaries.json", function(error, dc) {
 });
 
 d3.csv('data/schools.csv', function(data){
-	var scale = d3.scale.linear().domain([0,200]).range([1,5])
+  var scale = d3.scale.sqrt().range([1,10])
   svg.selectAll("circle")
     .data(data).enter().append("circle")
-      .attr("r", function(d) {return scale(d.enrollment)})
+      .attr("r", 4)
       .attr("fill-opacity", 0.5)
       .attr("fill", "#FF0000")
       .attr("transform", function(d) {
@@ -51,6 +51,25 @@ d3.csv('data/schools.csv', function(data){
           projection([d.long, d.lat]) +
           ")";});
 	packMetros();
+  d3.select("#school_enrollment").on("click", function() {changeSchoolData("enrollment")});
+  d3.select("#school_allocation").on("click", function() {changeSchoolData("alloc")});
+  d3.select("#school_location").on("click", noSchoolData);
+  function noSchoolData() {
+    svg.selectAll("circle")
+      .transition().duration(600)
+      .attr("r", 4)
+  }
+  function changeSchoolData(new_data_column) {
+    matchScaleToData(scale, function(d){return +d[new_data_column];})
+    svg.selectAll("circle")
+      .transition().duration(600)
+      .attr("r", function(d) {return scale(d[new_data_column])})
+  }
+  function matchScaleToData(scale, fieldFunction) {
+    var minimum = d3.min(data, fieldFunction),
+        maximum = d3.max(data, fieldFunction);
+    scale.domain([minimum, maximum]);
+  }
 });
 
 function packMetros() {
@@ -84,11 +103,3 @@ function clicked(d) {
 }
 
 }); // end document ready function
-
-function changeSchoolData(new_data) {
-  var scale = d3.scale.linear().domain([0,1000000]).range([1,5])
-  svg.selectAll("circle")
-    .transition().duration(600)
-    .attr("r", function(d) {return scale(d[new_data])})
-}
-
