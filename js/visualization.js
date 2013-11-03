19a1a555d656ed4ed367b365beda32f7d2c63639
Aaron Schumacher <ajschumacher@gmail.com>
2013-11-03 13:13:13 -0500

    manual merge of Nick's choropleth work into index.html etc.

--- js/visualization.js
+++ js/visualization.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 var width = 500,
     height = 500,
     centered;
+	
+var poverty_threshold = d3.scale.threshold()
+    .domain([.01, .17, .27, .38])
+    .range(["#e5ffc7", "#d9fcb9", "#bbef8e", "#9ad363", "#6eb43f"]);
+
+var diploma_threshold = d3.scale.threshold()
+    .domain([.06, .14, .19, .23])
+    .range(["#000", "#333", "#666", "#999", "#AAA"]);
 
 var packer = sm.packer();
 
@@ -27,21 +35,56 @@ var g = svg.append("g");
 g.append("g").attr("id", "neighborhoods");
 g.append("g").attr("id", "schools");
 
-d3.json("data/neighborhood_boundaries.json", function(error, dc) {
+queue()
+    .defer(d3.json, "data/neighborhood_boundaries.json")
+    .defer(d3.csv, "data/neighborhoods.csv")
+    .await(ready);
+
+function ready(error, dc, choropleth) {
+	var all_data = {};
+
+  choropleth.forEach(function(d) {
+	  all_data[d.gis_id] = d;
+	  choropleth[d.gis_id] = +d.poverty;
+  });
+
   g.select("#neighborhoods")
     .selectAll("path")
       .data(dc.features)
     .enter().append("path")
       .attr("d", path)
-      .on("click", clicked);
-  g.append("path")
-      .datum(topojson.mesh(dc, dc.features, function(a, b) { return a !== b; }))
-      .attr("id", "state-borders")
-      .attr("d", path);
-});
+      .on("click", clicked)
+      .style("fill", function(d) { return poverty_threshold(choropleth[d.properties.gis_id]); });
+  d3.select("#diploma").on("click", function() {changeNeighborhoodData("diploma")});
+  d3.select("#poverty").on("click", function() {changeNeighborhoodData("poverty")});
+  function noNeighborhoodData() {
+    g.select("#neighborhoods").selectAll("path")
+      .transition().duration(600)
+      .style("fill", "#333");
+  }
+  function changeNeighborhoodData(new_data_column) {
+	choropleth.forEach(function(d) {
+		choropleth[d.gis_id] = +d[new_data_column];
+	});
+	g.select("#neighborhoods").selectAll("path")
+      // .transition().duration(600)
+      .style("fill", function(d) { return diploma_threshold(all_data[d.properties.gis_id][new_data_column])})
+  }
+}
+
+
+//d3.json("data/neighborhood_boundaries.json", function(error, dc) {
+//  g.select("#neighborhoods")
+//    .selectAll("path")
+//      .data(dc.features)
+//    .enter().append("path")
+//      .attr("d", path)
+//      .on("click", clicked)
+//      .style("fill", function(d) { return color(choropleth[d.properties.gis_id]); });
+//});
 
 d3.csv('data/schools.csv', function(data){
-  var scale = d3.scale.sqrt().range([1,10])
+  var scale = d3.scale.sqrt().range([1,10]);
   g.select("#schools").selectAll("circle")
     .data(data).enter().append("circle")
       .attr("r", 4)
