7562974cfd23b4b538097722ebc9ae4ad9ee17f3
Margo <margo.lee.smith@gmail.com>
2014-02-25 02:39:23 -0500

    added chart, labels still overlap

--- js/visualization.js
+++ js/visualization.js
@@ -13,12 +13,22 @@ var defaultColor = "#aaa",
     fourColors = ["#e5ffc7", "#d9fcb9", "#bbef8e", "#6eb43f"],
     threeColors = ["#e5ffc7", "#bbef8e", "#6eb43f"];
 
+var chartSvg, labels, anchors, links;
+var chartMargin = {top: 30, right: 80, bottom: 30, left: 80};
+var chartWidth = 268, chartHeight = 150;
+var w = chartWidth - chartMargin.left - chartMargin.right;
+var h = chartHeight - chartMargin.top - chartMargin.bottom;
+var scale = d3.scale.linear().domain([0, 1]).range([h, 0]);
+var ord_scale = d3.scale.ordinal().domain(["Under 18", "Over 18"]).range([0, w]);
+
+
 $(document).ready(function() {
   init();
 }); // end document ready function
 
 function init(){
   drawChoropleth();
+  drawChart();
 
   // slide out menu
   $('.menu-toggle').on('click', toggleMenu);
@@ -261,6 +271,189 @@ function drawSchools(type){
   }
 }
 
+function drawChart(){
+
+  chartSvg = d3.select(".chart").append("svg").attr("width",chartWidth).attr("height",chartHeight)
+    .append("g")
+    .attr("transform","translate(" + chartMargin.left + "," + chartMargin.top + ")");
+
+  var color = d3.scale.category20();
+
+  var left_axis = d3.svg.axis().scale(scale).tickFormat("").orient("right").ticks(5);
+  var right_axis = d3.svg.axis().scale(scale).tickFormat("").orient("left").ticks(5);
+
+  chartSvg.append("g").attr("class","axis").call(left_axis)
+    .append("text").text("Under 18").attr("text-anchor","middle").attr("x",0).attr("y",-10);
+  
+  chartSvg.append("g").attr("class","axis").attr("transform","translate(" + w + ",0)").call(right_axis)
+    .append("text").text("Over 18").attr("text-anchor","middle").attr("x",0).attr("y",-10);
+
+  var ethdata = [
+    {name: "white", under18: 0.23,over18: 0.32},
+    {name: "black",under18: 0.60,over18: 0.55},
+    {name: "hispanic",under18: 0.10,over18: 0.06},
+    {name: "other",under18: 0.07,over18: 0.05}
+  ];
+
+  var ethnicity = chartSvg.selectAll(".ethnicity")
+      .data(ethdata)
+    .enter().append("g")
+      .attr("class","ethnicity");
+
+  ethnicity.append("line").attr("x1",0).attr("x2",w)
+      .attr("y1",function(d){ return scale(d.under18); })
+      .attr("y2",function(d){ return scale(d.over18); })
+      .style("stroke",function(d){ return color(d.name); })
+      .style("stroke-width",2); 
+
+  // Draw and adjust labels
+  var label_array = [];
+  var anchor_array = [];
+
+  for(i=0; i<4; i++){
+    var label = {x: - 10, y: scale(ethdata[i].under18), width: 0.0, height: 0.0, 
+      name: Math.round(ethdata[i].under18*100) + "% " + ethdata[i].name, ethnicity: ethdata[i].name, agegroup: "under18"};
+    label_array.push(label);
+    var label = {x: w + 10, y: scale(ethdata[i].over18), width: 0.0, height: 0.0,
+      name: Math.round(ethdata[i].over18*100) + "% " + ethdata[i].name, ethnicity: ethdata[i].name, agegroup: "over18"};
+    label_array.push(label);
+    
+    var anchor = {x: ord_scale('under18'), y: scale(ethdata[i].under18), r: 4, ethnicity: ethdata[i].name};
+    anchor_array.push(anchor);
+    var anchor = {x: ord_scale('over18'), y: scale(ethdata[i].over18), r: 4, ethnicity: ethdata[i].name};
+    anchor_array.push(anchor);
+  }
+  console.log(anchor_array);
+  
+  anchors = chartSvg.selectAll(".dot")
+    .data(anchor_array)
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("r", function(d) { return (d.r); })
+    .attr("cx", function(d) { return (d.x); })
+    .attr("cy", function(d) { return (d.y); })
+    .style("fill", function(d) { return color(d.ethnicity); });
+
+  labels = chartSvg.selectAll(".label")
+    .data(label_array).enter()
+    .append("text")
+    .attr("class", "label")
+    .attr("text-anchor", function(d) {
+      if(d.agegroup == "under18")
+        return "end";
+      else
+        return "start";
+    })
+    .attr("alignment-baseline","central")
+    .text(function(d) { return d.name; })
+    .attr("x", function(d) { return (d.x); })
+    .attr("y", function(d) { return (d.y); })
+    .attr("fill", function(d) { return color(d.ethnicity); });
+
+  var index = 0;
+  labels.each(function() {
+    label_array[index].width = this.getBBox().width;
+    label_array[index].height = this.getBBox().height;
+    index += 1;
+  });
+
+  links = chartSvg.selectAll(".link")
+    .data(label_array).enter()
+    .append("line")
+    .attr("class", "link")
+    .attr("x1", function(d) { return (d.x); })
+    .attr("y1", function(d) { return (d.y); })
+    .attr("x2", function(d) { return (d.x); })
+    .attr("y2", function(d) { return (d.y); });
+  
+  redrawLabels(label_array, anchor_array);
+}
+
+function updateChart(data){
+
+  var ethdata = [
+    {name: "white", under18: data.pop_nothisp_white_under18, over18: data.pop_nothisp_white},
+    {name: "black", under18: data.pop_nothisp_black_under18, over18: data.pop_nothisp_black},
+    {name: "hispanic", under18: data.pop_hisp_under18, over18: data.pop_hisp_under18},
+    {name: "other", under18: data.pop_nothisp_other_under18, over18: data.pop_nothisp_other}
+  ];
+
+  var label_array = [];
+  var anchor_array = [];
+
+  for(i=0; i<4; i++){
+    var label = {x: - 10, y: scale(ethdata[i].under18), width: 0.0, height: 0.0,
+      name: Math.round(ethdata[i].under18*100) + "% " + ethdata[i].name, ethnicity: ethdata[i].name, agegroup: "under18"};
+    label_array.push(label);
+    var label = {x: w + 10, y: scale(ethdata[i].over18), width: 0.0, height: 0.0,
+      name: Math.round(ethdata[i].over18*100) + "% " + ethdata[i].name, ethnicity: ethdata[i].name, agegroup: "over18"};
+    label_array.push(label);
+    
+    var anchor = {x: 0, y: scale(ethdata[i].under18), r: 4, ethnicity: ethdata[i].name};
+    anchor_array.push(anchor);
+    var anchor = {x: w, y: scale(ethdata[i].over18), r: 4,ethnicity: ethdata[i].name};
+    anchor_array.push(anchor);
+  }
+
+
+  setTimeout(function(){
+    chartSvg.selectAll(".ethnicity line")
+        .data(ethdata)
+        .transition()
+        .duration(1000)
+        .attr("y1",function(d){ return scale(d.under18); })
+        .attr("y2",function(d){ return scale(d.over18); });
+
+    chartSvg.selectAll(".label")
+      .data(label_array)
+      .transition()
+      .duration(1000)
+      .attr("y",function(d) { return d.y; })
+      .text(function(d) { return d.name; });
+
+    chartSvg.selectAll(".dot")
+      .data(anchor_array)
+      .transition()
+      .duration(1000)
+      .attr("cy",function(d) { return d.y; });
+
+    chartSvg.selectAll(".link")
+      .data(label_array)
+      .transition()
+      .duration(1000)
+      .attr("y1",function(d) { return d.y; })
+      .attr("y2",function(d) { return d.y; });
+  },0);
+
+  setTimeout(function(){
+    redrawLabels(label_array, anchor_array);
+  },1000);  
+}
+
+function redrawLabels(label_array, anchor_array) {
+
+ var sim_ann = d3.labeler()
+    .label(label_array)
+    .anchor(anchor_array)
+    .width(10)
+    .height(h)
+    sim_ann.start(1000);
+
+  labels
+  .transition()
+  .delay(1000)
+  .duration(800)
+  .attr("x", function(d) { return (d.x); })
+  .attr("y", function(d) { return (d.y); });
+
+  links
+  .transition()
+  .delay(1000)
+  .duration(800)
+  .attr("x2",function(d) { return (d.x); })
+  .attr("y2",function(d) { return (d.y); });
+}
+
 function changeSchoolData(new_data_column) {
   if (typeof new_data_column === 'string'){
     matchScaleToData(school_scale, function(d){return +d[new_data_column];})
@@ -336,6 +529,7 @@ function clicked(d) {
     //last neighborhood to display in popBox.
     activeId = d.properties.gis_id;
     setVisMetric(activeData, all_data[activeId][activeData]);
+    updateChart(all_data[activeId]);
   }
 }
 
