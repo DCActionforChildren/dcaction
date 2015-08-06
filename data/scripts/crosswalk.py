44ce65c5ff9cceb376e25cf7cccf1c2c07c2dfb3
ayota <ayota@outlook.com>
2015-08-05 20:16:45 -0400

    commit script outline

---
+++ data/scripts/crosswalk.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+#input: csv/json files / output: df
+def importfile():
+	#read in csvs
+
+	#if no geocol defined, find match on mapping column geoid
+
+
+#input: df / output: df
+def processing():
+	
+	#merge two files into one file with data cols, geoid_original, geoid_new, weight
+
+		#calculate weights if no weights column
+	
+	#multiply all data by weights column
+
+	#aggregate based on geoid_new
+
+#input: csvs/jsons / output: csv or json or w/e
+def mainfunction(datafile, mapfile, dftype, geocol):
+	#output new file
